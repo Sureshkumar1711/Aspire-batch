ccbb5bf9bf6d051fad932c6a998bc0572c080341
Admin <Admin@DESKTOP-VR7NCNH>
2022-04-01 08:21:53 +0530

    1-4

---
+++ Aspire/src/practice1/ExceptionDemo1.java
@@ -0,0 +1,26 @@
+package practice1;
+
+public class ExceptionDemo1 {
+
+	public static void main(String[] args) {
+		System.out.println("before exception...");
+		try {
+			int i=1/Integer.parseInt(args[0]);
+		}catch(ArithmeticException e) {
+			System.out.println(e);
+		}catch(ArrayIndexOutOfBoundsException ae) {
+			System.out.println(ae);
+		}
+////		catch(NumberFormatException ne) {
+////			
+////		}
+//		catch(Exception e) {
+//			System.out.println(e);
+//		}
+		finally {//finally is always called - whether exception arises or not / whether exception is caught or not
+			System.out.println("finally called...");
+		}
+		System.out.println("after exception...");
+	}
+}
+
